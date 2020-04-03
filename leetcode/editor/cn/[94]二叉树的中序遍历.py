dfd7db349c894fbfb23f0ab22abfe193618037e7
王根 <wanggen02@meituan.com>
2020-04-04 03:15:54 +0800

    ok

--- leetcode/editor/cn/[94]二叉树的中序遍历.py
+++ leetcode/editor/cn/[94]二叉树的中序遍历.py
@@ -17,13 +17,38 @@
 
 # leetcode submit region begin(Prohibit modification and deletion)
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return '%s %s, %s' % (self.val, self.left or '', self.right or '')
+
 
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
-        
-# leetcode submit region end(Prohibit modification and deletion)
+        rst = []
+        if not root: return rst
+        stack = []
+        while root or stack:
+            while root:
+                stack.append(root)
+                root = root.left
+            top = stack.pop()
+            rst.append(top.val)
+            root = top.right
+
+        return rst
+
+
+root = TreeNode(1)
+root.right = TreeNode(2)
+root.right.left = TreeNode(3)
+
+rst = Solution().inorderTraversal(root)
+print(rst)
